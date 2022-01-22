2c2b8e5438c19d0f15ca332518dce6090f62a12d
DEsimas <simasuh@gmail.com>
2022-01-22 20:47:30 +0300

    GetMessage type added

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,8 @@
+import { MessageOptions } from "discord.js";
+
 export interface Image {
     url: string;
     data?: Record<string, any>;
 };
+
+export type GetMessage = (images: Array<Image>, iterator: number, payload: any) => MessageOptions
