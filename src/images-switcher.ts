08f9914001cdb77affac1f3e9d38a8cff34a4686
DEsimas <simasuh@gmail.com>
2022-01-22 20:33:15 +0300

    necessary variables are described

--- src/images-switcher.ts
+++ src/images-switcher.ts
@@ -1,3 +1,19 @@
-export class ImagesSwitcher {
+import { Message, ReactionCollector } from "discord.js";
 
+export class ImagesSwitcher {
+    private readonly message: Message;
+    private readonly images: Array<any>;
+    private readonly botID: string;
+
+    private readonly getMessage: Function;
+    private readonly lifetime: number;
+    private readonly payload: any;
+
+    private readonly collector: ReactionCollector;
+
+    private readonly nextReaction = "➡️";
+    private readonly prevReaction = "⬅️";
+    private readonly stopReaction = "🛑";
+
+    private iterator: number = 0;
 };
