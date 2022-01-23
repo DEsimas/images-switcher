a0d29b4d3357c40837ca4bb252daf96ae14a19c1
DEsimas <simasuh@gmail.com>
2022-01-23 17:30:56 +0300

    default message getting function added

--- src/images-switcher.ts
+++ src/images-switcher.ts
@@ -1,4 +1,4 @@
-import { Message, ReactionCollector, MessageOptions, MessageReaction, User } from "discord.js";
+import { Message, ReactionCollector, MessageOptions, MessageReaction, User, MessageEmbed } from "discord.js";
 import { GetMessage, Image, SwitcherOptions } from "./types";
 
 export class ImagesSwitcher {
@@ -27,20 +27,22 @@ export class ImagesSwitcher {
         this.lifetime = options.lifetime || 1000*60*60*12;
         this.payload = options.payload;
 
-        this.collector = this.message.createReactionCollector(
-            (reaction, user) => (this.filter(reaction, user)),
-            { time: this.lifetime }
-        );
-
+        this.collector = this.message.createReactionCollector({
+            filter: (reaction, user) => (this.filter(reaction, user)),
+            time: this.lifetime
+        });
+        
         this.setReactions().then(() => {
             this.collector.on("collect", (reaction: MessageReaction, user: User) => (this.handleReaction(reaction, user)));
             this.collector.on("remove", (reaction: MessageReaction, user: User) => (this.handleReaction(reaction, user)));
             this.collector.on("end", () => (this.end()));
-        })
+        });
     }
 
     private async DefaultGetMessage(images: Array<Image>, iterator: number, payload: any): Promise<MessageOptions> {
-        return {};
+        const embed = new MessageEmbed()
+            .setImage(images[iterator].url);
+        return { embeds: [embed] };
     }
 
     private filter(reaction: MessageReaction, user: User): boolean {
